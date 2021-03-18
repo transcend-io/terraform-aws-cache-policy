2d8fe4943db7c72cda3bf9cfb367041b37ce064b
dmattia <david@transcend.io>
2021-03-18 11:13:10 -0500

    Add tf module

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # terraform-aws-cache-policy
+
 A terraform module for creating a CloudFront Cache Policy
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "id" {
+  value       = aws_cloudfront_cache_policy.policy.id
+  description = "The ID of the created policy"
+}
